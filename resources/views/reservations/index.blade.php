b2e91cd52b91ddfd09776741c60170389d1f66c9
hh <sshh83838@gmail.com>
2024-11-05 23:14:56 +0900

    feature-edit-create-reservation

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ $user->name }}様の今後の予定</h1>
+    <h1>あなたの今後の予定</h1>
+    <button type="button" class="btn btn-outline-secondary btn-block" onclick="location.href='{{ route('reservations.create') }}' ">予約追加</button>
     @if($reservations->isNotEmpty())
     <table>
         <thead>
             <tr>
-                <th>患者氏名</th>
                 <th>診療科</th>
                 <th>日付</th>
+                <th>編集</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($reservations as $reservation)
                 <tr>
-                    <td>{{ $reservation->users->name ?? 'N/A' }}</td> <!-- リレーションを使用 -->
-                    <td>{{ $reservation->categories->name ?? 'N/A' }}</td> <!-- リレーションを使用 -->
+                    {{-- <td>{{ $reservation->users->name ?? 'N/A' }}</td> --}}
+                    <td>{{ $reservation->categories->name ?? 'N/A' }}</td>
                     <td>{{ $reservation->date_formatted }}</td>
+                    <td><button type="button" class="btn btn-outline-secondary btn-block" onclick="location.href='{{ route('reservations.edit', $reservation) }}' ">編集</button></td>
                 </tr>
             @endforeach
         </tbody>
@@ -23,6 +25,4 @@
     @else
         <p>予約はありません。</p>
     @endif
-    <button class="btn" onclick="location.href='{{ route('reservations.create') }}' ">予約追加</button>
-    <button class="btn" onclick="location.href='{{ route('reservations.edit', $reservation) }}' ">編集</button>
 @endsection
